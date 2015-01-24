019a5cceca01072d15b2d24c8dcb46381933da07
andy <andyjiang@gmail.com>
2015-01-23 17:44:18 -0800

    added get request to fetchtext api

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,39 @@
+'use strict';
+
 (function() {
-  // do stuff.
+
+  getText('http://www.google.com');
+
+  /**
+   * Function to get text from a URL.
+   *
+   * @param {String} url.
+   */
+
+  function getText(url) {
+    var fetchtext = 'https://fetchtext-api.herokuapp.com/fetch/?url=' + url;
+    getURL(fetchtext, function(err, res) {
+      console.log(res);
+    });
+  }
+
+  /**
+   * Helper function to send GET request.
+   *
+   * @param {String} url
+   * @param {Function} cb
+   */
+
+  function getURL(url, cb) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+        cb(null, xmlhttp.responseText);
+      else
+        cb(xmlhttp.status, null);
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
 })();
